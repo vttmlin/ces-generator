4e69600a1127a242c5a4057b29ee8cab79d0371f
vttmlin <vttmlin.tmdaq.com>
2019-12-28 22:24:37 +0800

    commit

--- src/main/java/com/tmdaq/ces/generator/api/Const.java
+++ src/main/java/com/tmdaq/ces/generator/api/Const.java
@@ -1,5 +1,21 @@
 package com.tmdaq.ces.generator.api;
 
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static java.io.File.separator;
+import static java.util.stream.Collectors.joining;
+
 public class Const {
     public static final String PATH = "path.";
+
+    public static final String USER_DIR = System.getProperty("user.dir");
+
+    /**
+     * 判断当前是否是开发模式 如果是开发模式 会从读取当前的配置信息加载到内存中
+     */
+    public static final Boolean DEV_MODE = Objects.equals("true", System.getProperty("devMode"));
+
+    public static final String OUT_PATH = Stream.of(USER_DIR, "src", "main", "java").collect(joining(separator));
+
 }
